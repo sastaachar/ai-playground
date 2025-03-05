59eab63c3b74430c0bf1471251cf6e1ace6d6168
sastaachar <justinjohnmathew100@gmail.com>
2025-03-05 23:17:05 +0530

    a

--- server/update-rest-api.ts
+++ server/update-rest-api.ts
@@ -25,12 +25,12 @@ async function createRestApiData(): Promise<RestApiContent[]> {
     const methodName = parsedSpec.paths[path].get ? 'get' : 'post';
 
     const method = parsedSpec.paths[path][methodName]; 
-    const requestSchema = method?.requestBody?.content?.['application/json']?.schema;
+    const requestSchema = (method as any)?.requestBody?.content?.['application/json']?.schema;
     const requestType = requestSchema ? await compile(requestSchema, method.operationId+"Request", {
       bannerComment: `/* Request type for ${method.operationId} */`
      }) : "Doesnt take request body";
 
-    const responseSchema = method?.responses['200']?.content?.['application/json']?.schema;
+    const responseSchema = (method as any)?.responses['200']?.content?.['application/json']?.schema;
     const responseType = responseSchema ? await compile(responseSchema, method.operationId+"Response", {
       bannerComment: `/* Response type for ${method.operationId} */`
      }) : "Doesnt return anything";
@@ -53,8 +53,14 @@ async function createRestApiData(): Promise<RestApiContent[]> {
   return apiContents;
 }
 
-const convertRestDataToVector = (apiContents: RestApiContent[]) => {
- 
+const convertRestDataToVector = (apiContents: RestApiContent[]) => {  
+  const vectorEmbeddings = apiContents.map((apiContent) => {
+    return {
+      apiName: apiContent.apiName,
+    }
+  })
+
+  return vectorEmbeddings;
 }
 
 async function main() {
