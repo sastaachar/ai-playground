fe721a2399d96647d0a393a62e37e76f3acba735
sastaachar <justinjohnmathew100@gmail.com>
2025-03-06 12:10:48 +0530

    move onyx to fe

---
+++ src/services/onyx.ts
@@ -0,0 +1,68 @@
+import { config } from "../../server/constants";
+
+type OnyxResponse = {
+  chat_session_id: string;
+}
+
+export const createChatSession = async (): Promise<OnyxResponse> => {
+
+  const createChatSessionLink = `https://hubble.thoughtspot.dev/api/chat/create-chat-session`;
+
+  const response = await fetch(createChatSessionLink, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      persona_id: 12,
+    })
+  })
+  const data = await response.json();
+  console.log("Created chat session", data.chat_session_id);
+  return data;
+}
+
+
+const getOnyxResponse = async ({ query, chatSessionId, parentMessageId = null }: { query: string, chatSessionId?: string, parentMessageId?: number }) => {
+  if (!chatSessionId) {
+    console.log("Creating new chat session");
+    const response = await createChatSession();
+    chatSessionId = response.chat_session_id;
+  }
+
+
+  const sendMessageLink = `https://hubble.thoughtspot.dev/api/chat/send-message`;
+  return fetch(sendMessageLink, {
+    "headers": {
+      "content-type": "application/json",
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      alternate_assistant_id: 12,
+      chat_session_id: chatSessionId,
+      parent_message_id: parentMessageId,
+      message: query,
+      llm_override: {
+        model_provider: "Default",
+        model_version: "gpt-4o-mini"
+      },
+      prompt_id: 13,
+      search_doc_ids: null,
+      file_descriptors: [],
+      retrieval_options: {
+        run_search: "auto",
+        real_time: true,
+        filters: {
+          source_type: null,
+          document_set: null,
+          time_cutoff: null,
+          tags: []
+        }
+      }
+    }),
+  });
+}
+
+
+
+export { getOnyxResponse };
--- src/utils/openAi.ts
+++ src/utils/openAi.ts
@@ -1,99 +1,110 @@
 import { fetchEventSource } from "@microsoft/fetch-event-source";
 import { AI_MODEL } from "../../server/types";
 import { createSSEStreamForAiApi } from ".";
+import { getOnyxResponse } from "../services/onyx";
 
 const url = "/api/convo/ask";
 
 async function streamOpenAI({
-    query,
-    onData,
-    onChunk,
-    onComplete,
-  }: {
-    query: string;
-    onData: (data: any) => void;
-    onChunk?: (chunk: any) => void;
-    onComplete?: () => void;
-  }) {
-    const abortController = new AbortController();
-    const { signal } = abortController;
-    let content = "";
-    let retries = 0;
-    fetchEventSource(url, {
-      body: 
-         JSON.stringify({
-          query: query,
-          messages: [{
-            role: "system",
-            content: "You are a helpful assistant to web developers, trying to embed Thoughtspot in their application.",
-          }, {
-            role: "user",
-            content: `${query}`,
-          }],
-          model: 'onyx',
-          temperature: 0,
-          top_p: 0.9,
-          presence_penalty: 0,
-          frequency_penalty: 0,
-          stream: true,
-        }),
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      onmessage: (e) => {
-        try {
-          const chunk = parseEventData(e.data);
-          content += chunk?.choices?.[0].delta?.content|| "";
-          onData(content);
-          onChunk?.(chunk);
-  
-          if (chunk.done) {
-            onComplete?.();
-          }
-        } catch (e) {
-          console.error(e);
-        }
-      },
-      onclose: () => {
-         },
-      onerror: (e) => {
+  query,
+  onData,
+  onChunk,
+  onComplete,
+}: {
+  query: string;
+  onData: (data: any) => void;
+  onChunk?: (chunk: any) => void;
+  onComplete?: () => void;
+}) {
+  const abortController = new AbortController();
+  const { signal } = abortController;
+  let content = "";
+  let retries = 0;
+  fetchEventSource(url, {
+    body:
+      JSON.stringify({
+        query: query,
+        messages: [{
+          role: "system",
+          content: "You are a helpful assistant to web developers, trying to embed Thoughtspot in their application.",
+        }, {
+          role: "user",
+          content: `${query}`,
+        }],
+        model: 'onyx',
+        temperature: 0,
+        top_p: 0.9,
+        presence_penalty: 0,
+        frequency_penalty: 0,
+        stream: true,
+      }),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    onmessage: (e) => {
+      try {
+        const chunk = parseEventData(e.data);
+        content += chunk?.choices?.[0].delta?.content || "";
+        onData(content);
+        onChunk?.(chunk);
 
-        throw new Error("a")
-      },
-      onopen: async () => {
-        retries += 1;
-        if(retries > 3) {
+        if (chunk.done) {
           onComplete?.();
-          throw new Error("Failed to connect to the server");
         }
-      },
-      signal,
-    });
-  
-    return {
-      abort: () => abortController.abort(),
-    };
-  }
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    onclose: () => {
+    },
+    onerror: (e) => {
+
+      throw new Error("a")
+    },
+    onopen: async () => {
+      retries += 1;
+      if (retries > 3) {
+        onComplete?.();
+        throw new Error("Failed to connect to the server");
+      }
+    },
+    signal,
+  });
+
+  return {
+    abort: () => abortController.abort(),
+  };
+}
 
 function parseEventData(data: string) {
-    try {
-        return JSON.parse(data);
-    } catch {
-        return { content: '', done: false };
-    }
+  try {
+    return JSON.parse(data);
+  } catch {
+    return { content: '', done: false };
+  }
 }
-  
 
-const askApi = ({query, prevMessage, model = AI_MODEL.ONYX}: {query: string, prevMessage: any, model?: AI_MODEL}) => {
-  const isDebug = window.location.href.includes('debug=true');
+const askOnyx = ({ query, prevMessage }: { query: string, prevMessage: any }) => {
+  return getOnyxResponse({ query });
+}
+
+
+const askApi = ({ query, prevMessage, model = AI_MODEL.ONYX }: { query: string, prevMessage: any, model?: AI_MODEL }) => {
 
   const { host, token } = (window as any)._contentCache || {};
   const extraQuery = `Use ${host} as the thoughtspot host and use ${token} as the auth token for liveboardid use 74852035-9624-4fac-b352-200fa8506b14 `;
 
+  if (model === AI_MODEL.ONYX) {
+    return askOnyx({ query: query + extraQuery , prevMessage })
+  }
+
+  const isDebug = window.location.href.includes('debug=true');
+
+ 
   return fetch(url, {
-    body: 
-       JSON.stringify({
+    body:
+      JSON.stringify({
         query: query + extraQuery,
         direct: isDebug,
         prevMessage: prevMessage,
@@ -102,7 +113,8 @@ const askApi = ({query, prevMessage, model = AI_MODEL.ONYX}: {query: string, pre
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-    }});
+    }
+  });
 };
 
 // const askApi = async ({query, prevMessage, model = AI_MODEL.ONYX}: {query: string, prevMessage: any, model?: AI_MODEL}) => {
